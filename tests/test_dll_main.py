a2465ba7e9e33a75e7cc6d4ab298c27b482bec7c
Qin CHEN@p15 <qchen2015@hotmail.com>
2026-04-19 17:30:46 +0800

    test dllmain

--- tests/test_dll_main.py
+++ tests/test_dll_main.py
@@ -1,3 +1,5 @@
+import tempfile
+from pysgp4.AstroUtils import CreateCArray, c_double
 import pysgp4
 from ctypes import create_string_buffer
 sgp4 = pysgp4.DllMain
@@ -20,3 +22,29 @@ def test_dll_main_get_info():
     ).strip() == "HQ SpOC DllMain - Version: v9.8 - Build: Mar 04 2026 - Platform: Windows 64-bit - Compiler: OneAPI ifort"
 
 
+def test_get_MOIC_data():
+    moic_data = CreateCArray(c_double, [128])
+    sgp4.GetMOICData(128, moic_data)
+    print(moic_data[:128])  # print first 10 values for verification
+    assert True
+
+
+def test_Load_file():
+    filename = tempfile.NamedTemporaryFile(delete=False).name
+    data = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+    data_str = ", ".join(map(str, data))
+    with open(filename, "w") as f:
+        f.write(f"AS_MOIC {data_str}\n")
+    fn = create_string_buffer(filename.encode('utf-8'), 512)
+    retValue = sgp4.DllMainLoadFile(fn)
+    assert retValue == 0  # successfully loaded
+    moic_data = CreateCArray(c_double, [10])
+    sgp4.GetMOICData(10, moic_data)
+    print(moic_data[:10])  # print first 10 values for verification
+    assert list(moic_data[:10]) == data
+
+
+def test_log_file():
+    filename = create_string_buffer(b"test_log.txt", 256)
+    retValue = sgp4.OpenLogFile(filename)
+    assert retValue == 0  # successfully set log file
